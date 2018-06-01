5f5a89a3f37dae519a84450ab7af1068de88c0bd
ra61 <914466326@qq.com>
2018-06-01 16:50:20 +0800

    config

---
+++ config/index.js
@@ -0,0 +1,7 @@
+var config = {
+        development: require('./development.json'),
+        production: require('./production.json'),
+        test: require('./test.json')
+    };
+
+module.exports = config[process.env.NODE_ENV || 'development'];
